cd1a53e626facb8a38fd84de8f7ce91291180033
Bhdrpkcn <bhdrpkcn@gmail.com>
2025-02-07 10:50:32 +0300

    styling fix #2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import "./globals.css";
 import { Providers } from "@/store/Providers";
 
 export const metadata: Metadata = {
-  title: "bp-int",
-  description: "Practice technical interview questions",
+  title: "bp-QUIZ",
+  description: "An awesome quiz app",
 };
 
 export default function RootLayout({
@@ -14,6 +14,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/favicon-16x16.png" />
+      </head>
       <body className="bg-gray-100 text-gray-900">
         <Providers>
           <main className="container mx-auto p-4">{children}</main>
